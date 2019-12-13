a5be1b80dcb496128bc1263514ae4dfdded5d7ff
Stephen Olatunji <stephenolatunji02@gmail.com>
2019-12-13 12:03:55 +0100

    first commit

--- app.js
+++ app.js
@@ -4,3 +4,49 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const app = express();
+const port = process.env.PORT || 4004;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    password: String,
+    profession: String,
+    id: Number
+});
+const User = mongoose.model('User', userSchema);
+
+mongoose.connect('mongodb://localhost:27017/userDB', { useUnifiedTopology: true, useNewUrlParser: true });
+
+
+app.route('/users')
+    .get((req, res) => {
+        User.find((err, listUsers) => {
+            if (err) {
+                res.send(err)
+            }
+            res.send(listUsers)
+        });
+    })
+
+    .post((req, res) => {
+        const addUser = new User({
+            name: req.body.name,
+            password: req.body.password,
+            profession: req.body.profession,
+            id: req.body.id
+        });
+        addUser.save((err) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send('New user added');
+            }
+        });
+    });
+
+
+
+
